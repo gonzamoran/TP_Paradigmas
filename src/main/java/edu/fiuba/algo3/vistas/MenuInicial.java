6ac8c42beb27e192b0ae515967ceb14f7f171f81
MaxiPrantera <68716532+MaxiPrantera@users.noreply.github.com>
2025-11-26 18:35:34 -0300

    Menu Inicial del juego

---
+++ src/main/java/edu/fiuba/algo3/vistas/MenuInicial.java
@@ -0,0 +1,70 @@
+package edu.fiuba.algo3.vista;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MenuInicial extends VBox {
+
+    private final Stage stage;
+    private static final int ANCHO = 800;
+    private static final int ALTO = 550;
+
+    public MenuInicial(Stage stage) {
+        this.stage = stage;
+        this.stage.getIcons().add(new Image("Catan.png"));
+
+        this.setAlignment(Pos.CENTER);
+        this.setSpacing(20);
+        stage.centerOnScreen();
+
+        this.getStylesheets().add("styles.css");
+        this.getStyleClass().add("body");
+
+        Label etiqueta = this.crearEtiquetaDeBienvenida();
+        Button startButton = crearStartButton();
+        Button exitButton = crearExitButton();
+        HBox botonera = this.crearBotoneraHorizontal(startButton, exitButton);
+
+        this.getChildren().addAll(etiqueta, botonera);
+    }
+
+    private HBox crearBotoneraHorizontal(Button startButton, Button exitButton) {
+        HBox botonera = new HBox(startButton, exitButton);
+        botonera.setAlignment(Pos.CENTER);
+        botonera.setSpacing(20);
+        return botonera;
+    }
+
+    private Button crearExitButton() {
+        Button exitButton = new Button();
+        exitButton.setText("Salir");
+        exitButton.getStyleClass().add("exitButton");
+        exitButton.setOnAction(e -> {
+            ReproductorDeSonido.getInstance().playClick();
+            Platform.exit();
+        });
+        return exitButton;
+    }
+
+    private Button crearStartButton() {
+        Button startButton = new Button();
+        startButton.setText("Jugar");
+        startButton.getStyleClass().add("startButton");
+        startButton.setOnAction(e -> {
+            ReproductorDeSonido.getInstance().playClick();
+            Scene nuevaEscena = new Scene(new SeleccionCantidadJugadores(stage), ANCHO, ALTO);
+            stage.setScene(nuevaEscena);
+        });
+        return startButton;
+    }
+      private Label crearEtiquetaDeBienvenida() {
+        Label etiqueta = new Label("Bienvenidos a C.A.T.A.N.!");
+        etiqueta.getStyleClass().add("title");
+        return etiqueta;
+    }
+}
