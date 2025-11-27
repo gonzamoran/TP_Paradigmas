5c29fb48d7aad9824a5bdd1f42bb924d7d933f57
gonzamoran <gmoran@fi.uba.ar>
2025-11-27 20:09:11 -0300

    Agrego coso para los tests

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,5 @@ module edu.fiuba.algo3 {
     requires javafx.controls;
     requires json.simple;
     exports edu.fiuba.algo3;
-    exports edu.fiuba.algo3.vistas;
+    //exports edu.fiuba.algo3.vistas;
 }
