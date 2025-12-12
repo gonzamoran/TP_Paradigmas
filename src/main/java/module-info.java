01808b1785727df87aceb784e87a321bf25e7df7
MaxiPrantera <68716532+MaxiPrantera@users.noreply.github.com>
2025-12-12 16:42:19 -0300

    Add javafx.media

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 open module edu.fiuba.algo3 {
     requires javafx.controls;
-
+    requires javafx.media;
     exports edu.fiuba.algo3;
     exports edu.fiuba.algo3.vistas;
     exports edu.fiuba.algo3.modelo;
