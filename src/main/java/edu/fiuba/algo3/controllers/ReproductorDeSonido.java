0c92ae73366ec387aafc05ee6035806277560507
MaxiPrantera <68716532+MaxiPrantera@users.noreply.github.com>
2025-12-16 22:59:58 -0300

    Add sonido click y victoria

--- src/main/java/edu/fiuba/algo3/controllers/ReproductorDeSonido.java
+++ src/main/java/edu/fiuba/algo3/controllers/ReproductorDeSonido.java
@@ -9,11 +9,19 @@ import java.util.Objects;
 public class ReproductorDeSonido {
 
     private final String fAudioDados = "/sonidos/sonido_dados.mp3";
+    private final String fAudioClick = "/sonidos/botonClick.mp3";
+    private final String fAudioVictoria = "/sonidos/victoria.mp3";
+    
     private final AudioClip reproductorDados;
+    private final AudioClip reproductorClick;
+    private final AudioClip reproductorVictoria;
+
     private static ReproductorDeSonido instance;
 
     private ReproductorDeSonido() {
         this.reproductorDados = new AudioClip(Objects.requireNonNull(getClass().getResource(fAudioDados)).toString());
+        this.reproductorClick = new AudioClip(Objects.requireNonNull(getClass().getResource(fAudioClick)).toString());
+        this.reproductorVictoria = new AudioClip(Objects.requireNonNull(getClass().getResource(fAudioVictoria)).toString());
         this.reproductorDados.setVolume(0.5);
     }
 
@@ -23,7 +31,8 @@ public class ReproductorDeSonido {
         return instance;
     }
 
-    public void reproducirSonidoDados(){
-        reproductorDados.play();
-    }
+    public void reproducirSonidoDados(){reproductorDados.play(); }
+    public void playClick(){reproductorClick.play(); }
+    public void playVictoria() { reproductorVictoria.play(); }
 }
+
